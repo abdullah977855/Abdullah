b8b1f1e460e2a53ddd48b296cc54111fd371844b
abdullah977855 <abdullah75047@gmail.com>
2026-07-04 14:50:47 +0500

    JS updated

---
+++ JavaScript Folder/app.js
@@ -0,0 +1,21 @@
+// console.log("Hello Bhai");
+// console.warn("Hello Bhai3");
+// console.error("Login bhai") ;
+// console.table(["Abdullah", "Hannan", "Hassan"]);
+// alert("Freelancing Session\n Day : Saturday\n Timing : 7 to 9\n Campus : Autobhan");
+// alert("Freelancing Session\n Day : Sunday\n Timing : 12 to 2\n Campus : Autobhan");
+// prompt("Write your message");
+
+var userName = "Abdullah";
+var contactNo = 923554156874;
+var cnicNumber = 4130325648527;
+var webAndMobile = true;
+// console.log(userName);
+// console.log("My name is " + userName) // concatenation
+
+alert("Student name :" +userName+ "\nContact no :" +contactNo+ "\nCNIN Number :" +cnicNumber);
+
+console.log("Type of Student name : " +typeof(userName));
+console.log("Type of Student Contact : " +typeof(contactNo));
+console.log("Type of Student Cnic : " +typeof(cnicNumber))
+console.log("Type of Student Course : " +typeof(webAndMobile));

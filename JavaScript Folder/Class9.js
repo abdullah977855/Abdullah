ddee18d5f4b48268818f1ea665370d7ce1acf41a
abdullah977855 <abdullah75047@gmail.com>
2026-08-22 14:41:34 +0500

    Okay

---
+++ JavaScript Folder/Class9.js
@@ -0,0 +1,40 @@
+// let a = 4;
+// let b = 2;
+// let c = 27;
+// let process = Math.cbrt(c)
+// console.log(process);
+
+// const { use } = require("react");
+
+// Convert number to string
+// let a = 10;
+// console.log(a.toString());
+
+// String to Number
+// let a = "50";
+// console.log(Number(a));
+
+// let userInput = +prompt("Enter your number")
+// let round = Math.round(userInput);
+// let ceil = Math.ceil(userInput);
+// let floor = Math.floor(userInput);
+// let psTnt = parseInt(userInput);
+// let psFloat = parseFloat(userInput);
+// console.log("Your Input is: " + userInput + "\n Round: " + round + "\n Ceil: " + ceil + "\n Floor: " + floor + "\n ParseTnt: " + psTnt + "ParseFloat: " + psFloat)
+
+let createDate = new Date();
+console.log(createDate);
+
+// DATE 
+// var date = new Date() //date constructor
+// console.log(date)
+// console.log("Date : "+date.toString().substring(8,10))
+// console.log("Day : "+date.toString().substring(0,3))
+// console.log("Month : "+date.toString().substring(4,7))
+// console.log("Year : "+date.toString().substring(11,15))
+
+console.log(createDate.getFullYear());
+console.log(createDate.getMonth()+1);
+console.log(createDate.getDate());
+console.log(createDate.getDay());
+console.log(createDate.getTime());

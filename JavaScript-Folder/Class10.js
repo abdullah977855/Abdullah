e1f30a01c8562954b8de6b7add52f8dfa3720fb5
abdullah977855 <abdullah75047@gmail.com>
2026-08-23 15:00:00 +0500

    Okay

---
+++ JavaScript-Folder/Class10.js
@@ -0,0 +1,32 @@
+// var dateofBirth = prompt("Enter your date of birth (yyyy-mm-dd)");
+
+// var birth = new Date(dateofBirth);
+// var today = new Date();
+
+// var totalyear = today.getFullYear() - birth.getFullYear();
+// var totalmonth = today.getMonth() - birth.getMonth();
+// var totalday = today.getDate() - birth.getDate();
+
+// if (birth > today) {
+//     alert("Please enter correct date");
+// }
+// else {
+
+//     if (totalday < 0) {
+//         totalmonth--;
+//         totalday = totalday + 30;
+//     }
+
+//     if (totalmonth < 0) {
+//         totalyear--;
+//         totalmonth = totalmonth + 12;
+//     }
+
+//     document.write("Date of Birth: " + birth + "<br/>");
+//     document.write("Year: " + totalyear + "<br/>");
+//     document.write("Month: " + totalmonth + "<br/>");
+//     document.write("Day: " + totalday + "<br/>");
+// }
+
+
+let expiry = prompt("Enter expiry card");

05f30a13d74bc96e86684a9c0b422965bca54659
abdullah977855 <abdullah75047@gmail.com>
2026-09-06 14:44:47 +0500

    Code Updated

---
+++ JavaScript-Folder/Class11.js
@@ -0,0 +1,38 @@
+// function message(title){
+//     alert("=> " + title)
+// }
+
+// function setRed(event){
+// console.log(event);
+//     event.style.backgroundColor='red';
+// }
+
+// function setYellow(event){
+// console.log(event);
+//     event.style.backgroundColor='yellow';
+// }
+
+// let userName = document.getElementById("username");
+// let email = document.getElementById("email");
+// function submit(){
+//     if(userName.value == ""){
+//         userName.style.border = '2px solid red';
+//         alert("Fill out username")
+//     }else{
+//         userName.style.border = '2px solid black';
+//     }
+//     if(email.value == ""){
+//         email.style.border = '2px solid red';
+//         alert("Fill out email")
+//     }else{
+//         email.style.border = '2px solid black';
+//     }
+// }
+let textArea = document.getElementById("textarea");
+function setDesign(event){
+    if(event === "bold"){
+        textArea.style.fontWeight = 'bold';
+    }else if(event === "italic"){
+        textArea.style.fontStyle = 'italic';
+    }
+}
